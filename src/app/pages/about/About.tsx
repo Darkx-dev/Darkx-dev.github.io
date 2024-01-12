6d8d27a96908059f787a873227a619d10e76100d
Darkx23312 <130650239+Darkx-dev@users.noreply.github.com>
2024-01-12 05:37:42 +0530

    main

--- src/app/pages/about/About.tsx
+++ src/app/pages/about/About.tsx
@@ -7,12 +7,19 @@ export default function About() {
     <div className="text-white flex flex-col items-center w-4/5 max-sm:w-full mx-auto gap-10 pt-20">
       <h1 className="text-5xl font-bold">About</h1>
       <p className="text-lg max-sm:text-base text-center w-2/3 max-sm:w-full max-lg:w-full px-10 max-sm:px-5">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident
+        As a passionate coder and web developer specializing in frontend
+        technologies, I thrive on turning creative visions into seamless and
+        visually stunning digital experiences. With a meticulous approach to
+        coding and an eye for design, I am dedicated to crafting responsive and
+        user-friendly websites that leave a lasting impression. Proficient in
+        languages such as HTML, CSS, and JavaScript, I bring innovation to life
+        by implementing cutting-edge frameworks and staying abreast of industry
+        trends. My commitment to clean, efficient code ensures not only a
+        polished appearance but also optimal functionality. By seamlessly
+        blending aesthetics with functionality, I aspire to create immersive web
+        solutions that captivate users and elevate the online presence of
+        businesses and individuals alike. Let my skills and passion for frontend
+        development be the driving force behind your next digital endeavor.
       </p>
       <ul className="flex w-full justify-evenly text-lg max-sm:grid max-sm:grid-cols-1 max-sm:gap-10 max-sm:px-5 mt-5">
         <li className="flex flex-col items-center">
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,16 @@
-import React from 'react'
+import React from "react";
 
 export default function NavBar() {
   return (
-    <div className='nav-bar text-white'>
-        <div className="nav-belt subpixel-antialiased flex justify-between items-center my-8 mx-28 max-lg:mx-12  max-sm:justify-center max-sm:my-5">
-            <div className='font-bold text-4xl'>Darkx.dev</div>
-            <ul className="routes text-lg flex gap-10 max-sm:hidden">
-                <li>About me</li>
-                <li>Services</li>
-                <li>Contact me</li>
-            </ul>
-        </div>
+    <div className="nav-bar text-white">
+      <div className="nav-belt subpixel-antialiased flex justify-between items-center my-8 mx-28 max-lg:mx-12  max-sm:justify-center max-sm:my-5">
+        <div className="font-bold text-4xl">Darkx.dev</div>
+        <ul className="routes text-lg flex gap-10 max-sm:hidden">
+          <li className="hover:text-[#27AE60] transition-colors cursor-pointer">About me</li>
+          <li className="hover:text-[#27AE60] transition-colors cursor-pointer">Services</li>
+          <li className="hover:text-[#27AE60] transition-colors cursor-pointer">Contact me</li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
-
-
