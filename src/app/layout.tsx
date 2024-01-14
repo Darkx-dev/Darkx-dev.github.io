295c085ca34d186fa57ea98b980c0cda6887a5e8
Darkx23312 <130650239+Darkx-dev@users.noreply.github.com>
2024-01-14 14:42:19 +0530

    main

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="./profileCircle.png"/>
+        <link rel="icon" href="./favicon.ico"/>
       </head>
       <body className={inter.className}>
         <NavBar/>
